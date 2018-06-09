9cffbba067245bb39f96be5700be187c52b1af6e
Matthias Jakobs <m.jakobs265@gmail.com>
2018-06-09 17:47:48 +0200

    hides bad tweets (until a button is pressed)

--- tweetsense/tweetsense.js
+++ tweetsense/tweetsense.js
@@ -3,25 +3,33 @@ var allTweets = {}
 
 function getNewTweets() {
     var x = document.getElementsByClassName("tweet");
+    console.log(x.length)
     var i;
+    var added = 0;
     for (i = 0; i < x.length; i++) {
         const data_item_id = x[i].getAttribute("data-item-id")
-        if(!(allTweets[data_item_id] in allTweets)){
+        if(data_item_id != null && !(Object.keys(allTweets).includes(data_item_id))){
             var parsed = processRawTweet(x[i])
             object = {
                 id: data_item_id,
                 tweet: x[i],
                 tweetData: parsed,
-                alreadyProcesses: false
+                alreadyProcesses: false,
+                sentimentValue: (data_item_id % 1000)/500-1
             }
             allTweets[data_item_id] = object;
-            
+            added++;
+
             setTweetColor(
                 x[i],
-                sentinentColor((data_item_id % 1000)/500-1)
+                sentinentColor(object.sentimentValue)
             );
+
+            if(object.sentimentValue < -0.5) collapseTweet(x[i])
+
         }
     } 
+    console.log("added", added, "new items")
 }
 
 function processRawTweet(tweet){
@@ -33,6 +41,32 @@ function processRawTweet(tweet){
     return {author: a, text: t}
 }
 
-setInterval(getNewTweets, 1000)
+function collapseTweet(tweet){
+    var parentNode = tweet.parentNode
+    var toggleButton = document.createElement("button")
+
+    tweet.style.display = "none"
+
+    toggleButton.appendChild(document.createTextNode("let me see it"))
+    toggleButton.addEventListener("click", function (){
+        var siblings = parentNode.childNodes
+        for(var j = 0; j < siblings.length; j++){
+            if(siblings[j].classList && siblings[j].classList.contains("tweet")){
+                content = siblings[j]
+                if(content.style.display == "block"){
+                    content.style.display = "none"
+                } else {
+                    content.style.display = "block"
+                }
+            }
+        }
+    })
+
+    parentNode.appendChild(toggleButton)
+
+}
+
+getNewTweets()
+setInterval(getNewTweets, 10000)
 
 setTimeout( () => console.log(allTweets), 5000)
