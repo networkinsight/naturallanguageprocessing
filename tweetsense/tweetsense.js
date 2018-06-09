a0ec13903db51f2d5b6b111588abb8855663f098
Matthias Jakobs <m.jakobs265@gmail.com>
2018-06-09 14:49:24 +0200

    added simple parsing functionality

--- tweetsense/tweetsense.js
+++ tweetsense/tweetsense.js
@@ -1,21 +1,32 @@
-tweetData = {}
-//x[i].style.backgroundColor = "red";
+var allTweets = {}
 
 function getNewTweets() {
-
     var x = document.getElementsByClassName("tweet");
     var i;
     for (i = 0; i < x.length; i++) {
         const data_item_id = x[i].getAttribute("data-item-id")
-        object = {
-            id: data_item_id,
-            tweet: x[i],
-            alreadyProcesses: false
+        if(!(allTweets[data_item_id] in allTweets)){
+            var parsed = processRawTweet(x[i])
+            object = {
+                id: data_item_id,
+                tweet: x[i],
+                tweetData: parsed,
+                alreadyProcesses: false
+            }
+            allTweets[data_item_id] = object;
         }
-        tweetData[data_item_id] = object;
     } 
+}
+
+function processRawTweet(tweet){
+    var t = tweet.getElementsByClassName("js-tweet-text")[0].innerHTML
+    t = t.replace(/<a.*\/a>/i, "")
+
+    var a = tweet.getElementsByClassName("fullname")[0].innerHTML
+
+    return {author: a, text: t}
 }
 
 setInterval(getNewTweets, 1000)
 
-setTimeout( () => console.log(tweetData), 5000)
+setTimeout( () => console.log(allTweets), 5000)
